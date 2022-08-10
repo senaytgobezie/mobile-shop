f479a52db235ac00e64bc3cdb31ca102e3702f48
senayt gobezie <senaitgobezie@gmail.com>
2022-08-10 10:33:12 +0300

    new price completed

--- index.js
+++ index.js
@@ -34,7 +34,7 @@
     // filter items on button click
     $(".button-group").on("click", "button", function(){
         var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue});
+        $grid.isotope({ filter: "."+filterValue});
     })
 
 
